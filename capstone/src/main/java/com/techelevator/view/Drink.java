2e6ad9942a77dce39afcc5e990d515c1377eab03
tacey <taceywhittemore@gmail.com>
2022-04-13 11:55:55 -0700

    -m "Drinks Class started. TW"

--- capstone/src/main/java/com/techelevator/view/Drink.java
+++ capstone/src/main/java/com/techelevator/view/Drink.java
@@ -1,4 +1,39 @@
 package com.techelevator.view;
 
-public class Drink implements Purchasable{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Drink implements Purchasable {
+     private String slot;
+     private String name;
+     private double price;
+     private int quantity;
+
+     public Drink (String slot, String name, double price, String type, int quantity) {
+          this.slot = slot;
+          this.name = name;
+          this.price = price;
+          this.quantity = quantity;
+     }
+     public int minusPurchase(){
+          quantity = quantity -1;
+          return quantity;
+     }
+
+     public String getSlot() {
+          return slot;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public double getPrice() {
+          return price;
+     }
+
+     public int getQuantity() {
+          return quantity;
+     }
 }
